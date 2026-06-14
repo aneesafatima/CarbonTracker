865417073b85f0b15349bbe299c0b7421fbb08c8
aneesa 🌟 <161751452+aneesafatima@users.noreply.github.com>
2026-06-14 15:17:45 +0530

    fix:used correct class name for fitness screen

--- lib/features/fitness/screens/main_screen.dart
+++ lib/features/fitness/screens/main_screen.dart
@@ -13,7 +13,7 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
 
-  List<Widget> screens = [FitnessMetricsScreen(), MapScreen(), ProfileScreen()];
+  List<Widget> screens = [FitnessMetrics(), MapScreen(), ProfileScreen()];
 
   @override
   Widget build(BuildContext context) {
